732e5a9e447da33fe29ee9042d97d449725241c0
Mantas Boronilščikovas <mantas.boronilscikovas@swedbank.lt>
2022-03-09 16:08:40 +0200

    feat: immutable task

--- src/main/java/com/academy/service/CustomerService.java
+++ src/main/java/com/academy/service/CustomerService.java
@@ -19,11 +19,29 @@ public class CustomerService {
     public void insert(Customer customer) {
         validator.validate(customer);
 
-        customer.setFirstName(capitalizeFirstLetter(customer.getFirstName()));
-        customer.setLastName(capitalizeFirstLetter(customer.getLastName()));
-        customer.setPersonalNumber(formatPersonalNumber(customer.getPersonalNumber()));
+        customer = formatCustomer(customer);
 
         repository.insert(customer);
+    }
+
+    private Customer formatCustomer(Customer customer) {
+        String firstName = capitalizeFirstLetter(customer.getFirstName());
+        String lastName = capitalizeFirstLetter(customer.getLastName());
+        String personalNumber = formatPersonalNumber(customer.getPersonalNumber());
+
+        Customer.Builder customerBuilder = new Customer.Builder(
+                firstName,
+                lastName,
+                personalNumber);
+
+
+        customerBuilder
+                .withAge(customer.getAge())
+                .withCity(customer.getCity())
+                .withCountryCode(customer.getCountryCode())
+                .withMiddleName(customer.getMiddleName());
+
+        return customerBuilder.build();
     }
 
     private String formatPersonalNumber(String personalNumber) {
