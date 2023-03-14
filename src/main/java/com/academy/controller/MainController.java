e254746e8dd41020b0af499fbe8454fec8c1516a
Mantas Boronilščikovas <mantas.boronilscikovas@swedbank.lt>
2023-03-14 14:01:56 +0200

    feat: first task

--- src/main/java/com/academy/controller/MainController.java
+++ src/main/java/com/academy/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.academy.controller;
 
 import com.academy.entity.Customer;
+import com.academy.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,20 +11,21 @@ import java.util.List;
 @RequestMapping("/api")
 public class MainController {
 
-    public MainController() {
-        
-    }
+    @Autowired
+    private CustomerRepository customerRepository;
 
     @RequestMapping("/customer/all")
     public List<Customer> fetchCustomers() {
-        return null;
+        return customerRepository.findAll();
     }
 
     @PostMapping("/customer/insert")
     public void insertCustomer(@RequestBody Customer customer) {
+        customerRepository.insert(customer);
     }
 
     @DeleteMapping("/customer/delete/{customerId}")
     public void deleteCustomer(@PathVariable String customerId) {
+        customerRepository.deleteById(customerId);
     }
 }
